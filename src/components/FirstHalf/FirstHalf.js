f44b9b98d964b3c36b93cd7b102211592ccf21dd
ibrahim <megatey97@gmail.com>
2022-01-07 19:10:03 +0100

    top page space

--- src/components/FirstHalf/FirstHalf.js
+++ src/components/FirstHalf/FirstHalf.js
@@ -2,14 +2,14 @@ import './FirstHalf.css'
 
 const FirstHalf = () => {
     return (
-        <div className="firsthalf">
+        <div className="firsthalf" id="Home">
             <div className="container">
                 <div className="dark">
-                <div className="content">
+                    <div className="content">
                         <h3>Hi!, I'm Ibrahim</h3>
                         <h1>Web developer <span>From Nigeria.</span></h1>
                         <p>I'm developer focused on crafting user‑friendly experiences, I am passionate about building excellent software that improves the lives of those around me.</p>
-                </div>
+                    </div>
                 </div>
             </div>
         </div>
